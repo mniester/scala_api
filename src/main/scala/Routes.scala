47f029c9376a8a76c7ab2bc40523706f9446eab0
Misza <mniester@gmail.com>
2022-05-05 15:21:21 +0200

    Added route to separate object

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -1,3 +1,12 @@
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+
 object Routes {
-  
+  val hello =
+      path("hello") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>***** ***</h1>"))
+        }
+      }
 }
--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,9 +1,9 @@
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
 import scala.io.StdIn
+
+import Routes._
 
 object Server {
 
@@ -13,14 +13,7 @@ object Server {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.executionContext
 
-    val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-        }
-      }
-
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(hello)
 
     println(s"Server now online. Please navigate to http://localhost:8080/hello\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
