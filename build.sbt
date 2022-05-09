f4379b1e665ec284a24d0980695b2de5a74ab8ce
Misza <mniester@gmail.com>
2022-05-09 15:05:10 +0200

    first test route prototype

--- build.sbt
+++ build.sbt
@@ -18,5 +18,6 @@ libraryDependencies ++= Seq("org.scalactic" %% "scalactic" % "3.2.10",
                             "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
                             "com.github.jwt-scala" %% "jwt-core" % "9.0.5",
                             "io.jvm.uuid" %% "scala-uuid" % "0.3.1",
-                            "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion
+                            "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
+                            "com.typesafe.akka" %% "akka-http-testkit" % AkkaHttpVersion % "test"
                             )
--- src/test/scala/TestRoutes.scala
+++ src/test/scala/TestRoutes.scala
@@ -1,12 +1,24 @@
-import scala.concurrent.{Future, Await}
-import akka.actor.ActorSystem
-import akka.http.scaladsl.client.RequestBuilding.Post
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.Http
+import akka.event.NoLogging
+import akka.http.scaladsl.model.ContentTypes._
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.stream.scaladsl.Flow
+import org.scalatest.flatspec.AsyncFlatSpec
+import org.scalatest.matchers.should.Matchers
 
 import Settings._
 import Routes._
 
-object RoutesTests {
-  ???
+class ServiceSpec extends AsyncFlatSpec with Matchers with ScalatestRouteTest {
+  // Get(s"/ip/${ip2Info.query}") ~> routes ~> check {
+  //     status shouldBe OK
+  //     contentType shouldBe `application/json`
+  //     responseAs[IpInfo] shouldBe ip2Info
+  //   }
+  val testUser = "aaa"
+
+  Get(s"user${testUser}") ~> userGet ~> check {
+    status shouldBe OK
+  }
 }
