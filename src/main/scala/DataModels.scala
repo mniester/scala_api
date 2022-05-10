0bb98844eec4a33ce206c82fed6f8cc2ad017b16
Misza <mniester@gmail.com>
2022-05-10 18:22:56 +0200

    TaskModel: variables  => functions

--- src/main/scala/DataModels.scala
+++ src/main/scala/DataModels.scala
@@ -8,6 +8,8 @@ import DBs.DBFacade
 
 case class JWTModel(key: Int, uuid: String, name: String)
 
+//case class ErrorModel
+
 abstract class AbstractProjectModel (key: Int, name: String, user: Int, startTime: String, deleteTime: String = "")
 
 case class UserModel(key: Int, uuid: String, name: String)
@@ -43,10 +45,10 @@ case class ProjectModelWithTasks (key: Int,
 
 case class TaskModel(key: Int, name: String, user: Int, startTime: String, endTime: String, project: Int, volume: Int, comment: String, deleteTime: String) {
   
-  lazy val startMoment = LocalDateTime.parse(startTime)
-  lazy val endMoment = LocalDateTime.parse(endTime)
-  lazy val duration = Duration.between(startMoment, endMoment).getSeconds()
-  val numberOfChars: Int = key.toString.length + name.length +  Settings.maxUserNameLength + startTime.length + endTime.length + volume.toString.length + comment.length + deleteTime.length
+  def startMoment(): LocalDateTime = LocalDateTime.parse(startTime)
+  def endMoment(): LocalDateTime = LocalDateTime.parse(endTime)
+  def duration(): Long = Duration.between(startMoment, endMoment).getSeconds()
+  def numberOfChars(): Int = key.toString.length + name.length +  Settings.maxUserNameLength + startTime.length + endTime.length + volume.toString.length + comment.length + deleteTime.length
 
   def checkLocalTimeDateOverlap (otherTask: TaskModel): Boolean =
     !this.endMoment.isBefore(otherTask.startMoment) && !otherTask.endMoment.isBefore(this.startMoment)
